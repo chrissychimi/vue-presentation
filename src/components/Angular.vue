f4d3f3f18a34a830dcee2799a00afd77bf5f2557
Christina Chimi <christina.chimi@nrg.com>
2017-05-23 17:03:13 -0400

    Fill out components I will need

---
+++ src/components/Angular.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div id="angular">
+      <h2>Angular</h2>
+      <ul>
+        <li>Uses Typescript extensively.</li>
+        <li>One Right Way</li>
+        <li>Designed for large, complex apps.</li>
+      </ul>
+    </div>
+    <div id="vuejs">
+      <h2>VueJS</h2>
+      <ul>
+        <li>Doesn't require Typescript (but can use).</li>
+        <li>Smaller.</li>
+        <li>Relatively unopinionated.</li>
+        <li>Shallower learning curve.</li>
+      </ul>
+    </div>
+  </div>
+</template>
---
+++ src/components/React.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div id="react">
+      <h2>React</h2>
+      <ul>
+  			<li>If you want to write a for loop, you write a for loop. You don't have to go looking in the docs for how to set up a v-for.</li>
+  			<li>Tooling.</li>
+  			<li>CSS may be complicated, CSS-in-JS (not something I am in the know about).</li>
+  			<li>Richer ecosystem of developers.</li>
+  			<li>Native support for mobile apps.</li>
+  			<li>Embraces functional programming and immutability (although with Vuex Vue becomes more like this).</li>
+      </ul>
+    </div>
+    <div id="vuejs">
+      <h2>VueJS</h2>
+      <ul>
+  			<li>Optimization (keeps track of which children depend on which props and doesn't re-render needlessly).</li>
+  			<li>If you want to write HTML, you write HTML. No need to translate the familiar.</li>
+  			<li>Migrating existing projects. You could bring your static HTML pages right into Vue, adding reactivity as you get more comfortable w/ the framework.</li>
+  			<li>Non-dev designers.</li>
+  			<li>CSS is easy and scopable (unique data attributes).</li>
+  			<li>Official support for main companion libraries.</li>
+  			<li>CLI that allows user templates (React is working on this).</li>
+  			<li>Shallower learning curve.</li>
+  			<li>Working with Weex (Alibaba) to develop cross-platform support.</li>
+  			<li>Mutability does have some benefits, particularly for readability.</li>
+      </ul>
+    </div>
+  </div>
+</template>
